c76b336be3386f62ecaaa3cf3aee225599f4a791
Mustafa OZCERIT <mustafa.ozcerit@bilgeadam.com>
2023-05-13 20:27:50 +0300

    #Added mersis identity check

---
+++ Sigortam.Cerit/Controllers/MernisController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sigortam.Cerit.Controllers
+{
+    public class MernisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async void IdentityCheck()
+        {
+            var client = new MernisService.KPSPublicSoapClient(MernisService.KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            try
+            {
+                var response = await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(13991590581), "MUSTAFA", "ÖZCERİT", 1997);
+                var result = response.Body.TCKimlikNoDogrulaResult;
+            }
+            catch (Exception ex)
+            {
+                Exception.Equals(ex.Message, "İnternet bağlantınızı kontrol edin");
+            }
+            var aaa = 5;
+            //return result;
+        }
+    }
+}
---
+++ Sigortam.Cerit/wwwroot/js/mersis.js
@@ -0,0 +1,26 @@
+﻿function identityCheck() {
+        debugger;
+        let $form = $("#form");
+        if ($form.length === 0) {
+            return null;
+        }
+        var data = {
+            Name: document.getElementById("name").value, LastName: document.getElementById("lastName").value, IdentificationNumber: document.getElementById("identificationNumber").value, BirthDate: document.getElementById("birthDate").value,
+            PlateNumber: document.getElementById("plateNumber").value
+        };
+
+        $.post("Mersis/IdentityCheck", { userPostDto: data}, function (result) {
+            debugger;
+            if (result.code == 101) {
+                location.href = "/Home/Index";
+            }
+            else {
+                location.href = "/UserLogin/Index";
+            }
+            //if (result == "Succeeded") {
+            //    $(".card-alert .close").click(function () {
+            //        $(this).closest(".card-alert").fadeOut("slow");
+            //    });
+            //}
+        });
+    }
