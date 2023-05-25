86106a26d564bba8de4958d7c08516b238c1789e
Mustafa OZCERIT <mustafa.ozcerit@bilgeadam.com>
2023-05-25 14:56:20 +0300

    add globalcontst

---
+++ Sigortam.Cerit.Common/GlobalConstruction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigortam.Cerit.Common
+{
+    public class GlobalConstruction
+    {
+        public const string WebSiteName = "Sigortam.Cerit";
+    }
+}
