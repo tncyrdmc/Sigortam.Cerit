1a70131284fa2fda3c66b5e4c111415d203f33b4
Aycan TULUK <aycan.tuluk@bilgeadam.com>
2023-05-25 14:28:31 +0300

    login account

---
+++ Sigortam.Cerit/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sigortam.Cerit.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        } 
+        [HttpPost]
+        public IActionResult Login(string username)
+        {
+            return View();
+        }
+    }
+}
