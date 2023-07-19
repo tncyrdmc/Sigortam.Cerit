26e644495d754fcea659601decd3a73336e17708
Mustafa OZCERIT <mustafa.ozcerit@bilgeadam.com>
2023-07-19 12:07:13 +0300

    add dashboard

---
+++ Sigortam.Cerit/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sigortam.Cerit.Core.Interfaces;
+
+namespace Sigortam.Cerit.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        #region cash
+        private IInsurance _servis;
+        #endregion
+
+        #region ctor
+        public DashboardController(IInsurance servis)
+        {
+            _servis = servis;
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            var thisMonthInsurances = _servis.GetInsurances().Where(x => x.InsuranceStartDate.Month == DateTime.Now.Month);
+
+            var peopleCount = thisMonthInsurances.Select(x => x.User.UserId).Distinct().Count();
+
+            var insuranceCount = thisMonthInsurances.Count();
+
+            var totalAmount = thisMonthInsurances.Sum(x => x.Price);
+
+            ViewBag.TotalAmount = totalAmount;
+            ViewBag.InsuranceCount = insuranceCount;
+            ViewBag.PeopleCount = peopleCount;
+
+            return View();
+        }
+    }
+}
